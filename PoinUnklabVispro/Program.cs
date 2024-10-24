4c302655c92cfaf79606a131f4f2e7d211221221
Jordannst <s22310272@student.unklab.ac.id>
2024-10-24 14:22:19 +0800

    change

--- PoinUnklabVispro/Program.cs
+++ PoinUnklabVispro/Program.cs
@@ -11,6 +11,7 @@ namespace PoinUnklabVispro
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        /// Testtt
         [STAThread]
         static void Main()
         {
